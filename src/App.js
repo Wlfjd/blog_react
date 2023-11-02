9d1befa7f974847318205bdb135db18233316123
Wlfjd <leejiwon813@naver.com>
2023-11-02 23:05:31 +0900

    반복문으로 post 배열 수 만큼 생성, post에 따른 모달창 구현

--- src/App.js
+++ src/App.js
@@ -7,27 +7,53 @@ import './App.css';
 function App() {
   let [title,setTitle]=useState(['남자 코드 추천','강남 우동 맛집','파이썬 독학'])
   let [like,setLike]=useState(0)
-  let [modal,setModal]= useState(false) //일단 닫힌 상태로 구현
+  //let [modal,setModal]= useState(false) //일단 닫힌 상태로 구현
+  let [value,setValue]=useState(null)
+
+
   return (
-    <div className="App">
+  <div className="App">
     <div className="black-nav">
-        <div>Blog</div>
-      </div>
-      <div className="list" onClick={()=> setModal(!modal)}>
-        <h4>{title[0]} <span onClick={()=>setLike(like+1)}>👍</span>{like} </h4>
-        <p>2월 17일 발행</p>
-      </div>
-      {modal && <Modal /> }
+      <div>Blog</div>
     </div>
+    {title.map((data,i)=>{
+      return(
+        <>
+        <div className="list" key={i}>
+          <h4 
+            onClick={()=>{
+            if(value==null) setValue(i)
+            else if(value===i) setValue(null)
+
+           }}> {data} 
+           <span onClick={()=>{
+            setLike(like+1)}}>👍</span> {like} </h4>
+          <p>2월 17일 발행</p>
+        </div>
+        {value===i && 
+        <Modal 
+         value={i}
+         title={title} 
+         color={'lightblue'} 
+         onchange={setTitle}/> }
+        </>
+      )
+    })}
+  </div>
   );
 }
 
-function Modal(){
+function Modal({value, color,onchange,title}){
   return(
-      <div className="modal">
-        <h4>제목</h4>
+      <div className="modal" style={{background: color}}>
+        <h4>{title[value]}</h4>
         <p>날짜</p>
         <p>상세내용</p>
+        <button onClick={()=>{
+          let copy=[...title]
+          copy[0]='수정'
+          onchange(copy)
+        }}>글 수정</button>
       </div>
   )
 }
